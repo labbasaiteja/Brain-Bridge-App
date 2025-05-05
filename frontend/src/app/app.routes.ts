002486c01303accebceb5e2ecb1fee30c4dc22d1
divya malyala <divyamalyala23@gmail.com>
2025-05-04 23:22:14 -0500

    routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
-import { LayoutComponent } from './layout/layout.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfessorProfileComponent } from './app/professor-profile/professor-profile.component';
+import { LayoutComponent } from './layout/layout.component';
 import { ViewApplicantsComponent } from './view-applicants/view-applicants.component';
 
 export const routes: Routes = [
@@ -19,6 +22,24 @@ export const routes: Routes = [
       { path: 'view-applicants', component: ViewApplicantsComponent }
 
       // You can add other children routes like 'view-applicants' here
-    ]
+    ],
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'professor-profile',
+    component: ProfessorProfileComponent
   }
 ];
